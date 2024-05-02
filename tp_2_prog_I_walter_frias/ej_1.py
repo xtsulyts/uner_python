ecb00a610a8f6052689e839a9d4db4deb4102c18
xtsulyts <531xtsulyts@gmail.com>
2024-05-02 19:32:07 -0300

    user fedora

---
+++ tp_2_prog_I_walter_frias/ej_1.py
@@ -0,0 +1,13 @@
+###### FUNCIONES #####
+
+# 1. Que al pasarle una cadena <nombre> nos muestre por pantalla el saludo ¡Hola <nombre>!.
+
+def saludo(nombre):
+    print("##### EJERCICIO 1 #####")
+    print("")
+    print(f"¡Hola {nombre}!")
+
+
+saludo(input("Escriba su nombre: "))
+print("")
+print("##### FIN DEL PROGRAMA #####")
