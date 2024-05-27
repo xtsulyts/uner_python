d66f89f48bc3f2f954881a5a6952a0d6f38df190
xtsulyts <531xtsulyts@gmail.com>
2024-05-27 18:22:20 -0300

    Avances ej_4 user_fedora

--- tp_3_prog_I_walter_frias_2024/ej_3.py
+++ tp_3_prog_I_walter_frias_2024/ej_3.py
@@ -11,18 +11,25 @@ def agrega_lista():
     i = 0
     lista = ["banana", "manzana", "pera"]
     lista_nueva = []
+    print(f"Lista de frutas: \n {lista}")
     
     while i < 3:
-        ingreso = input("Ingrese el nombre de una fruta: ")
+        ingreso = input("Ingrese el nombre de una fruta a la lista: ")
+        ingreso = ingreso.lower()
         existente = ingreso in lista or lista_nueva
         if existente == True:
-            print("ingrese otro nombre:")
+            print(f"La fruta {ingreso} ya existe ingrese otra:")
         else:
             lista.append(ingreso)
             lista_nueva.append(ingreso)
             i += 1
     matriz = list(lista), list(lista_nueva)
-    for i in matriz:
-        print(i)
+    for x in matriz:
+        print(x)
+    print()
             
 agrega_lista()
+
+print("")
+print("#### FIN DEL PROGRAMA ####")
+print("")
---
+++ tp_3_prog_I_walter_frias_2024/ej_4.py
@@ -0,0 +1,11 @@
+""" 4. Dada la siguiente lista: países = [“Argentina,”Brasil”, “Bolivia”,”Paraguay”,”Venezuela”], realizar lo siguiente: 
+a. Imprimir la cantidad de elementos que tiene la lista
+b. Imprimir el primer y último elemento. 
+c. Imprimir el resto. 
+d. Permitir que el usuario ingrese un país e imprimir el índice si el país se encuentra en la lista. Si no se encuentra, imprimir un mensaje advirtiendo al usuario. 
+e. Permitir al usuario ingresar un número igual o menor a la cantidad de elementos de la lista. Quitar el elemento correspondiente de esa posición. 
+f. Imprimir la lista en orden inverso. 
+g. Vaciar la lista."""
+
+def ej_4 ():
+    pass
