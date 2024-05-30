746e2d06753b2f143b941698b2fe7efe07777205
xtsulyts <531xtsulyts@gmail.com>
2024-05-29 22:26:40 -0300

    Avances ej_6 user_win_11

---
+++ tp_3_prog_I_walter_frias_2024/ej_6.py
@@ -0,0 +1,14 @@
+""" 6. Programe una aplicación de consola Python que brinde al usuario la posibilidad de a partir de una lista vacía: 
+a. Agregar un elemento al final. 
+b. Agregar un elemento al principio. 
+c. Quitar un elemento al final. 
+d. Quitar un elemento al principio"""
+
+def ej_6 ():
+    lista_vacia = []
+    agregar_elementos = input("Ingresar elemento: ")
+    elemento_final = lista_vacia.append(agregar_elementos)
+    #lista_vacia = elemento_final
+    print(lista_vacia)
+
+ej_6()
