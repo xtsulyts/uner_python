ac7a06a564c5d65053a29e9502f9753da97ae9e0
xtsulyts <531xtsulyts@gmail.com>
2024-08-16 18:36:18 -0300

    Cabio nombre ej tp

--- tp_1_prog_II_walter_frias_2024/S1_Ej_a.py
+++ tp_1_prog_II_walter_frias_2024/S1_Ej_a.py
@@ -2,32 +2,22 @@
 letras_prohibidas), la cual retorne True si es que los caracteres que componen
 una palabra no se encuentran en una lista de caracteres prohibidos."""
 
-"""def palabra_no_tiene_letras(palabra, letras_prohibidas):
-    palabra = True
-    while palabra in letras_prohibidas:
-        
-        return True
-    print("ok")
-    return True
-
-   
-palabra_no_tiene_letras(palabra=input("Ingrese una palabra: "), letras_prohibidas = ["a", "b", "c"])"""
-
+print("###############")
+print("### ejer. a ###")
+print("###############")
 def palabra_no_tiene_letras(palabra, letras_prohibidas):
     for letra in palabra:
         if letra in letras_prohibidas:
             return False
     return True
 
-# Definimos la palabra y las letras prohibidas
-palabra = "holaz"
+palabra = input("Ingrese una palabra: ")
 letras_prohibidas = ['x', 'y', 'z']
 
-# Llamamos a la función y almacenamos el resultado
 resultado = palabra_no_tiene_letras(palabra, letras_prohibidas)
 
-# Imprimimos el resultado
-print(resultado)  # Esto imprimirá True
+print(resultado)
+print("Fin de la ejecución")
 
 
     
